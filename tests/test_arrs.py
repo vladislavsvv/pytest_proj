14cde39c22bf79c77a5ec771d3089d47aad3f659
vladislav <vladisla.sv.299563@gmail.com>
2023-07-22 17:19:29 +0600

    fix test file

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,15 @@
-from utils import arrs
+from utils.arrs import get, my_slice
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert get([1, 2, 3], 1, "test") == 2
+    assert get([], 0, "test") == "test"
+    assert get([1, 2, 3], -1, "test") == "test"
 
 
 def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert my_slice([1, 2, 3], 1) == [2, 3]
+    assert my_slice([]) == []
+    assert my_slice([1, 2, 3], -1, 3) == [3]
+    assert my_slice([1, 2], -3) == [1, 2]
